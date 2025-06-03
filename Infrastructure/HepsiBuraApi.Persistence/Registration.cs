c3c6facda7a438e56df00d08949aa9e62a3a8f20
naki-agc <nakiagca10@gmail.com>
2025-06-03 21:55:25 +0300

    IWriteRepository-WriteRepository Added

---
+++ Infrastructure/HepsiBuraApi.Persistence/Registration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using HepsiBuraApi.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using HepsiBuraApi.Application.Interface.Repositories;
+using HepsiBuraApi.Persistence.Repositories;
+
+namespace HepsiBuraApi.Persistence
+{
+    public static class Registration
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+          services.AddDbContext<AppDbContext>(options =>
+          {
+              options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+          });
+
+            services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+        }
+    }
+}
