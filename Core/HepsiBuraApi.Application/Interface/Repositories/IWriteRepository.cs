e1922647e5b16e8143fa91e6b81d02af60f1b7f1
naki-agc <nakiagca10@gmail.com>
2025-06-04 15:20:52 +0300

    UnitOfWork Added

--- Core/HepsiBuraApi.Application/Interface/Repositories/IWriteRepository.cs
+++ Core/HepsiBuraApi.Application/Interface/Repositories/IWriteRepository.cs
@@ -9,11 +9,12 @@ using System.Threading.Tasks;
 
 namespace HepsiBuraApi.Application.Interface.Repositories
 {
-    public interface IWriteRepository<T> where T : class , IEntityBase, new()
+    public interface IWriteRepository<T> where T : class, IEntityBase, new()
     {
         Task AddAsync(T entity);
         Task AddRangeAsync(IList<T> entities);
         Task UpdateAsync(T entity);
         Task HardDeleteAsync(T entity);
+    }
 
 }
---
+++ Core/HepsiBuraApi.Application/Interface/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using HepsiBuraApi.Application.Interface.Repositories;
+using HepsiBuraApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuraApi.Application.Interface.UnitOfWork
+{
+    public interface IUnitOfWork:IAsyncDisposable
+    {
+
+        IReadRepository<T> GetReadRepository<T>() where T : class, IEntityBase, new();
+        IWriteRepository<T> GetWriteRepository<T>() where T : class, IEntityBase, new();
+
+        Task<int> SaveAsync();
+        Task<int> Save();
+    }
+}
