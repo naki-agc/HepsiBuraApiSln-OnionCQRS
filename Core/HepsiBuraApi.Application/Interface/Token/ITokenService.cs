dc950f43192fe13b79fa4d5a8fcf50535b1e9384
naki-agc <nakiagca10@gmail.com>
2025-07-23 21:20:56 +0300

    Jwt ve CreateToken işlemleri yazıldı

---
+++ Core/HepsiBuraApi.Application/Interface/Token/ITokenService.cs
@@ -0,0 +1,21 @@
+﻿using HepsiBuraApi.Domain.Entities;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiBuraApi.Application.Interface.Token
+{
+    public interface ITokenService
+    {
+        Task<JwtSecurityToken> CreateToken(User user, IList<string> roles); // Kullanıcı ve roller ile JWT token oluşturur.
+
+        string GenerateRefreshToken(); // Yeni bir refresh token oluşturur.
+
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);// Token'ın süresi dolmuşsa, token'dan ClaimsPrincipal oluşturur.
+    }
+}
